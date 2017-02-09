b9061062893b5f427514b911627991c8c143d261
abaranov <abaranov@student.unit.ua>
2017-02-09 14:02:55 +0200

    sad

--- get_next_line.c
+++ get_next_line.c
@@ -1,81 +1,97 @@
-//
-// Created by Aleksandr Baranov on 1/26/17.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaranov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/06 16:26:33 by abaranov          #+#    #+#             */
+/*   Updated: 2017/02/06 16:42:17 by abaranov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft/libft.h"
 
-int check_n(char *str)
+int						check_n(char *str)
 {
-    int i;
+	int					i;
 
-    i = 0;
-    while(str[i])
-        if(str[i++] == '\n')
-            return(0);
-    return(1);
+	i = 0;
+	while (str[i])
+		if (str[i++] == '\n')
+			return (0);
+	return (1);
 }
 
-char *start(int fd, char *str) {
-    char *temp;
+char					*start(int fd, char *str)
+{
+	char				*temp;
+	char				*freea;
 
-    temp = ft_strnew(BUFF_SIZE);
-    ft_bzero(temp, BUFF_SIZE);
-
-    if (read(fd, temp, BUFF_SIZE) > 0)
-    {
-        //printf("its TEMP afer read: %s\n", temp);
-        str = ft_strjoin(str, temp);
-        free(temp);
-        if (check_n(str))
-            return start(fd, str);
-    }
-
-    return (str);
+	temp = ft_strnew(BUFF_SIZE);
+	ft_bzero(temp, BUFF_SIZE);
+	if (read(fd, temp, BUFF_SIZE) > 0)
+	{
+		freea = ft_strdup(str);
+		free(str);
+		str = ft_strjoin(freea, temp);
+		free(freea);
+		free(temp);
+		if (check_n(str))
+			return (start(fd, str));
+	}
+	return (str);
 }
 
-
-int get_next_line(const int fd, char **line)
+t_struct				*change_list(int fd, t_struct **head)
 {
-    char *str;
-    int i;
-    int t;
-    static char *temp;
+	t_struct			*tmp;
 
-    if(fd < 0 || !line || read(fd, NULL, 0) == -1 || BUFF_SIZE < 0)
-        return (-1);
-    t = 0;
-    i = 0;
+	tmp = *head;
+	if (tmp != NULL)
+	{
+		while ((tmp != NULL) && (tmp->fd != fd))
+			tmp = tmp->next;
+		if (tmp && tmp->fd == fd)
+			return (tmp);
+	}
+	if (tmp == NULL)
+	{
+		tmp = malloc(sizeof(t_struct));
+		tmp->fd = fd;
+		tmp->balance = "\0";
+		tmp->next = NULL;
+	}
+	*head != NULL ? tmp->next = *head : 0;
+	*head = tmp;
+	return (*head);
+}
 
-    if(temp) {
-        str = temp;
-    }
-    else
-        str = ft_strnew(BUFF_SIZE);
-    temp = ft_strnew(BUFF_SIZE);
+int						get_next_line(const int fd, char **line)
+{
+	char				*str;
+	int					i;
+	int					t;
+	static t_struct		*temp;
+	t_struct			*res;
 
-    ft_bzero(temp, ft_strlen(temp));
-
-    if(check_n(str))
-        str = start(fd, str);
-
-    *line = ft_strnew(ft_strlen(str));
-    ft_bzero(*line, ft_strlen(*line));
-    //  if(str[i] == '\n') i++;
-    while(str[i] != '\n' && str[i]) {
-        (*line)[t++] = str[i];
-        i++;
-    }
-    t = 0;
-    if(str[i] == '\n') i++;
-    while(str[i])
-    {
-        temp[t] = str[i];
-        t++;
-        i++;
-    }
-    free(str);
-    if(i == 0)
-        return (0);
-    return (1);
+	if (fd < 0 || !line || read(fd, NULL, 0) == -1 || BUFF_SIZE < 0)
+		return (-1);
+	t = 0;
+	i = 0;
+	res = change_list(fd, &temp);
+	str = (*(res->balance)) ? res->balance : ft_strnew(BUFF_SIZE);
+	res->balance = ft_strnew(BUFF_SIZE);
+	check_n(str) ? str = start(fd, str) : 0;
+	*line = ft_strnew(ft_strlen(str));
+	while (str[i] != '\n' && str[i])
+		(*line)[t++] = str[i++];
+	t = 0;
+	str[i] == '\n' ? i++ : 0;
+	while (str[i])
+		res->balance[t++] = str[i++];
+	free(str);
+	if (i == 0)
+		return (0);
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,17 +1,29 @@
-//
-// Created by Aleksandr Baranov on 1/26/17.
-//
-#include "fcntl.h"
-#include "stdlib.h"
-#include "stdio.h"
-#include "libft/libft.h"
-#define BUFF_SIZE 10000
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaranov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/06 16:53:15 by abaranov          #+#    #+#             */
+/*   Updated: 2017/02/06 16:58:53 by abaranov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-typedef struct  s_struct
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "stdlib.h"
+# include "libft/libft.h"
+# define BUFF_SIZE 11
+
+typedef struct		s_struct
 {
-    int fd;
-    char *line;
-    struct t_struct* next;
-}                       t_struct;
+	int				fd;
+	char			*balance;
+	struct s_struct	*next;
+}					t_struct;
 
-int get_next_line(const int fd, char **line);
+int					get_next_line(const int fd, char **line);
+
+#endif
